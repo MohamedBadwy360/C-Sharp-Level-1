08af32db149932bed8e30d6cffacd5287a23762e
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-03 22:56:58 +0200

    Nullable Types

---
+++ 10. Nullable Types/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _10.Nullable_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Nullable<int> can be assigned any value
+            //  from -2147483648 to 2147483647, or a null value.
+
+            Nullable<int> i = null;
+
+            //Console.WriteLine(i);
+        }
+    }
+}
