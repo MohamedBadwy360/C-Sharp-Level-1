be6290787aa12c6e060011b079efa613b8b4f612
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-04 00:57:30 +0200

    DateTime Static Fields

---
+++ 15. Ticks/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _15.Ticks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //number of 100-nanosecond intervals that have elapsed
+            //since January 1, 0001, at 00:00:00.000 in the Gregorian calendar.
+
+            DateTime dt = new DateTime();
+            Console.WriteLine(DateTime.MinValue.Ticks);
+            Console.WriteLine(DateTime.MaxValue.Ticks);
+        }
+    }
+}
