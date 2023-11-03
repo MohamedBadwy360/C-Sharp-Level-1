c1cb6ded16f9964a465e71be5ebff700a943ed4e
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-04 01:02:53 +0200

    Time Span

---
+++ 17. TimeSpan/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _17.Time_Span
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2023, 11, 4);
+
+            // Hours, Minutes, Seconds
+            TimeSpan ts = new TimeSpan(49, 30, 30);
+            Console.WriteLine(ts);
+            Console.WriteLine(ts.Days);
+            Console.WriteLine(ts.Hours);
+            Console.WriteLine(ts.Minutes);
+            Console.WriteLine(ts.Seconds);
+
+            DateTime newDate = dt.Add(ts);
+            Console.WriteLine(newDate);
+        }
+    }
+}
