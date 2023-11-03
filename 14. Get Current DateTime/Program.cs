de990c44a6ea8653efb9e83f03f558ec778ce810
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-04 00:49:15 +0200

    Get Current DateTime

---
+++ 14. Get Current DateTime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.Get_Current_DateTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //assigns default value 01/01/0001 00:00:00
+            DateTime dt1 = new DateTime();
+
+            dt1 = DateTime.Now;
+            Console.WriteLine(dt1);
+        }
+    }
+}
