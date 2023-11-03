c55587d1c50d215f4cf2d8e3e617a1e082f6d6a3
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-03 03:44:50 +0200

    WriteLine

---
+++ 01. WriteLine/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01.WriteLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("My Name is Mohamed.");
+            Console.WriteLine("My Score is 10 + 20 : " + (10 + 20));
+        }
+    }
+}
