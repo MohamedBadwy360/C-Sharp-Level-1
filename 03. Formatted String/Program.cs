2eb03c13e352426afd916a7fd2a9db14a194b2e7
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-03 04:28:53 +0200

    Formatted String

---
+++ 03. Formatted String/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _03.Formatted_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} {1}.", "Welcome", "To Egypt");
+            Console.WriteLine("My Name is {0}. I Live in {1}.", "Mohamed", "Egypt");
+        }
+    }
+}
