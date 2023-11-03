93ffd690c9bdcbdb51c145f346e8bc1b0b6fbc2c
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-04 01:24:01 +0200

    Interpolated String

---
+++ 22. String Interpolation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _22.String_Interpolation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  String Interpolation
+
+            string firstName = "Mohammed";
+            string lastName = "Abu-Hadhoud";
+            string code = "107";
+
+            //You shold use $ to $ to identify an interpolated string 
+            Console.WriteLine($"Mr. {firstName} {lastName}, code: {code}");
+
+            string fullName = $"Mr. {firstName} {lastName}, code: {code}";
+            Console.WriteLine(fullName);
+        }
+    }
+}
