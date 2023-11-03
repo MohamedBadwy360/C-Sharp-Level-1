194140b9fdc63ffe930968a69c2491167a3b6a71
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-03 23:40:59 +0200

    Dynamic Type

---
+++ 12. Dynamic Type/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _12.Dynamic_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic MyDynamicVar = 100;
+            Console.WriteLine("value: {0}, type: {1}\n", MyDynamicVar, MyDynamicVar.GetType());
+
+            MyDynamicVar = "Hello World!!";
+            Console.WriteLine("Value: {0}, Type: {1}\n", MyDynamicVar, MyDynamicVar.GetType());
+
+            MyDynamicVar = true;
+            Console.WriteLine("Value: {0}, Type: {1}\n", MyDynamicVar, MyDynamicVar.GetType());
+
+            MyDynamicVar = DateTime.Now;
+            Console.WriteLine("Value: {0}, Type: {1}\n", MyDynamicVar, MyDynamicVar.GetType());
+
+        }
+    }
+}
