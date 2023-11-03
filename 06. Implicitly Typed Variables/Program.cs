bb440904c82d0c635c47721cc2d26b11eed617e7
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-03 04:41:52 +0200

    Implicitly Typed Variables

---
+++ 06. Implicitly Typed Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _06.Implicitly_Typed_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Implicitly typed variables
+             Alternatively in C#, we can declare a variable without knowing 
+             its type using var keyword. 
+             Such variables are called implicitly typed local variables.
+
+             Variables declared using var keyword must be initialized at the time of declaration.
+            */
+
+            var x = 10;
+            var y = 20.5;
+            var z = "Mohamed";
+
+            Console.WriteLine("x = {0}, y = {1}, z = {2}", x, y, z);
+        }
+    }
+}
