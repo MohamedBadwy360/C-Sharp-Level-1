f2638d28e5a30b5ca5657596bc66e135531e34db
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-03 04:24:52 +0200

    Write

---
+++ 02. Write/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02.Write
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("My Name is Mohamed. ");  // Like cout in c++
+            Console.Write("I Love Programming with C#. ");
+            Console.Write("I Love Desktop and Web Applications.");
+        }
+    }
+}
