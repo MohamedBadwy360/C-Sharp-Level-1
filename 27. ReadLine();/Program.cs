f3973f994aef020312860dedb480940010c69b74
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-04 02:26:19 +0200

    ReadLine();

---
+++ 27. ReadLine();/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _27.ReadLine___
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.ReadLine() always reads string.
+
+            // Type your username and press enter
+            Console.WriteLine("Enter username?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Username is: " + userName);
+        }
+    }
+}
