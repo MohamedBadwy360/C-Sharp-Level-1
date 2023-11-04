a34817163af6ac49e70582ad6b8dec4d912575e8
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-04 04:36:31 +0200

    Random Function In C#

---
+++ 46.  Random Function In C#/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _46.Random_Function_In_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            for (byte i = 0; i < 4; i++)
+            {
+                Console.WriteLine(random.Next(10, 20)); // returns random integers >= 10 and < 20
+            }
+        }
+    }
+}
