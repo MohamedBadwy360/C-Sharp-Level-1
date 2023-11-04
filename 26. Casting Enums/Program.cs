6ea6c23f18d05c2e795adec6552f27a113038245
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-04 02:23:54 +0200

    Casting Enums

---
+++ 26. Casting Enums/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _26.Casting_Enums
+{
+    internal class Program
+    {
+        enum WeekDays
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WeekDays.Friday); //output: Friday 
+            int day = (int)WeekDays.Friday;
+            Console.WriteLine(day);             //output: 4 
+
+            var wd = (WeekDays)5;               // int to enum conversion
+            Console.WriteLine(wd);              //output: Saturday 
+        }
+    }
+}
