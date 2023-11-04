c8f2da09cafca57d3c20363577a8acf9c633c822
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-04 04:32:49 +0200

    Exceptions

---
+++ 45. Exceptions/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _45.Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] myNumbers = { 1, 2, 3 };
+                Console.WriteLine(myNumbers[10]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
