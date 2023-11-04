90bcf04f09c5b128cb3c15f76bfe0b969ff89688
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-04 02:16:26 +0200

    Explicit Casting

---
+++ 24. Explicit Casting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _24.Explicit_Casting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double myDouble = 17.58;
+            int myInt = (int) myDouble;     // double myDouble = 17.58;
+
+            Console.WriteLine(myDouble);   // Outputs 17.58
+            Console.WriteLine(myInt);      // Outputs 17
+        }
+    }
+}
