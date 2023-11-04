863005a7f1dd4b9a35ee885e773030f1037cc804
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-04 03:47:06 +0200

    Count , Sum And Average Using System.Linq

---
+++ 42.  Count , Sum And Average on Array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+// provides us various methods to use in an array
+using System.Linq;
+
+
+namespace _42.Count___Sum_And_Average_on_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Note that we used System.Linq above.
+
+            int[] numbers = { 20, 22, 19, 18, 1 };
+
+
+
+            // compute Count
+            Console.WriteLine("Count = " + numbers.Count());
+
+            // compute Sum
+            Console.WriteLine("Sum = " + numbers.Sum());
+
+            // compute the average
+            Console.WriteLine("Average = " + numbers.Average());
+        }
+    }
+}
