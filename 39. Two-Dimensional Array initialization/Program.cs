e918ac7421f0dadaea4cd7cd94e85af89bb86c72
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-04 03:36:04 +0200

    Two-Dimensional Array initialization

---
+++ 39. Two-Dimensional Array initialization/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _39.Two_Dimensional_Array_initialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //initializing 2D array
+            int[,] numbers = { { 12, 9 }, { 10, 11 } };
+
+            // access first element from the first row
+            Console.WriteLine("Element at index [0, 0] : " + numbers[0, 0]);
+
+            // access first element from second row
+            Console.WriteLine("Element at index [1, 0] : " + numbers[1, 0]);
+
+        }
+    }
+}
