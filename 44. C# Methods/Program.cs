a97fbf192421f735e9cf5572a3088c568843dc13
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-04 04:22:20 +0200

    C# Methods

---
+++ 44. C# Methods/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _44.C__Methods
+{
+    internal class Program
+    {
+        static void PrintName()
+        {
+            Console.WriteLine("My Name is Mohamed");
+        }
+        static void Main(string[] args)
+        {
+            PrintName();
+        }
+    }
+}
