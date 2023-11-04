237ae9bc8785bad5c63c1515995538ee95593a7a
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-04 03:25:56 +0200

    continue Statement

---
+++ 37. continue Statement/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Main
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 5; ++i)
+            {
+
+                if (i == 3)
+                {
+                    continue;
+                }
+
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
