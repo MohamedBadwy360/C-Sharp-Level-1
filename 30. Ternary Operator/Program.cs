c87b05c7dedab700d40aded08881346696f7bb8e
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-04 02:38:10 +0200

    Ternary Operator

---
+++ 30. Ternary Operator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _30.Ternary_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 12;
+            string result;
+
+            result = (number % 2 == 0) ? "Even Number" : "Odd Number";
+            Console.WriteLine("{0} is {1}", number, result);
+        }
+    }
+}
