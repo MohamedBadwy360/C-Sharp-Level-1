6aa89839b99c063186f61fb6debddab9c7260e46
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-05 23:43:34 +0200

    Drawing

---
+++ 50. Drawing/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _50.Drawing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Color Black = Color.FromArgb(255, 0, 0, 0);
+            Pen pen = new Pen(Black);
+            pen.Width = 10;
+
+            pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+            e.Graphics.DrawLine(pen, 100, 100, 100, 200);
+            e.Graphics.DrawRectangle(pen, 200, 200, 400, 400);
+            e.Graphics.DrawEllipse(pen, 200, 50, 100, 120);
+        }
+    }
+}
