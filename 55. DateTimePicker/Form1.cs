6749b58cd59c9b59423c8b6c6b7714e3c0966a16
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-07 08:04:27 +0200

    Date Time Picker and Month Calendar

---
+++ 55. DateTimePicker/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _55.DateTimePicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(dateTimePicker1.Value.ToShortDateString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(dateTimePicker1.Value.ToLongDateString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(monthCalendar1.SelectionRange.ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(monthCalendar1.SelectionRange.Start.ToShortDateString());
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(monthCalendar1.SelectionRange.End.ToShortDateString());
+        }
+    }
+}
