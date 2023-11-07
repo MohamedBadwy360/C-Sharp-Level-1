77f2784d250dc8c95e82d78adb52fe01d9d6ffd7
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-07 09:46:03 +0200

    TreeView and ImageList

---
+++ 58. TreeView and ImageList/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _58.TreeView_and_ImageList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void treeView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show(treeView1.SelectedNode.Text);
+        }
+
+        private void treeView1_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            CheckTreeViesNode(e.Node, e.Node.Checked);
+        }
+
+        private void CheckTreeViesNode(TreeNode node, bool isChecked)
+        {
+            foreach (TreeNode item in node.Nodes)
+            {
+                item.Checked = isChecked;
+
+                if (item.Nodes.Count > 0)
+                {
+                    this.CheckTreeViesNode(item, isChecked);
+                }
+            }
+        }
+    }
+}
