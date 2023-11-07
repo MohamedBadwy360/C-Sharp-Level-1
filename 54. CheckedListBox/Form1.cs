73dfecd43d766adccc7cf5afe6eb089a81e1039f
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-07 08:04:10 +0200

    Checked List Box

---
+++ 54. CheckedListBox/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _54.CheckedListBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (byte i = 1; i <= 5; i++)
+            {
+                checkedListBox1.Items.Add("Items" + i);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (byte i = 0; i < checkedListBox1.CheckedItems.Count; i++)
+            {
+                MessageBox.Show(checkedListBox1.CheckedItems[i].ToString());
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (byte i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, true);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.RemoveAt(2);
+        }
+    }
+}
